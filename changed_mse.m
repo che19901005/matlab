43acf0d56517c3bd8ccab7323ea2fd3296947879
che19901005 <hangyu.che@gmail.com>
2013-08-12 15:59:08 +0800

    some test versions
    add some failed test versions

---
+++ changed_mse.m
@@ -0,0 +1,33 @@
+% this algotiehm is:
+% calculate Lni for each view.
+% initialize ai as 1/m.
+% for each Lni, calculate Yi for each view(eigenvectors with smallest eigenvalues)
+% repeat:
+% Y = sum(ai^r*Yi)
+% update ai
+% until convergence.
+function Y = changed_mse(d,data, r, k )
+    import MSE;
+    mse = MSE(d,data,r,k);
+    Lni = mse.Ln;
+    Yi = {};
+    for i = 1:length(Lni)
+        Yi{i} = mse.get_Y_through_L(Lni{i});
+    end
+    Y = zeros(d,mse.number_instances);
+    for i = 1: length(Yi)
+        Y = Y + mse.ai(i)^r * Yi{i}; 
+    end
+    while(mse.update_and_check_convergence(Y) == 0)
+         for i = 1: length(Yi)
+            Y = Y + mse.ai(i)^r * Yi{i}; 
+         end
+    end
+     for i = 1: length(Yi)
+            Y = Y + mse.ai(i)^r * Yi{i}; 
+     end
+     disp('final ai is:');
+     disp(mse.ai);
+end
+% result is almost the same as the standard one.
+% we can say that Y = sum(ai^r*Yi)
--- scatter_for_input_matrix.m
+++ scatter_for_input_matrix.m
@@ -9,9 +9,9 @@ function scatter_for_input_matrix(input_matrix)
     two_index = find(labels == 2,1);
     three_index = find(labels == 3,1);
     four_index = find(labels == 4,1);
-    s = 20;
-    scatter(input_matrix(1,1:two_index-1),input_matrix(2,1:two_index-1),s,'red','+'); hold on
-    scatter(input_matrix(1, two_index: three_index-1),input_matrix(2,two_index: three_index-1),s,'blue','c'); hold on
-    scatter(input_matrix(1,three_index: four_index-1),input_matrix(2,three_index: four_index-1),s, 'green', 'x'); hold on
-    scatter(input_matrix(1,four_index:length(labels)),input_matrix(2,four_index:length(labels)),s,'black','s');
+    s = 40;
+    %scatter(input_matrix(1,1:two_index-1),input_matrix(2,1:two_index-1),s,'red','+'); hold on
+    scatter(input_matrix(1, 1: three_index-1),input_matrix(2,1: three_index-1),s,'blue','c','filled'); hold on
+    %scatter(input_matrix(1,three_index: four_index-1),input_matrix(2,three_index: four_index-1),s, 'green', 'x'); hold on
+    scatter(input_matrix(1,three_index:length(labels)),input_matrix(2,three_index:length(labels)),s,'black','s','filled');
 end
